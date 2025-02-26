b8f5deefdcca4d880cf24d706b5fe9cc25b83f4a
fireclouu <jasperaquino1412@gmail.com>
2025-02-26 17:20:56 +0800

    Update init.lua with fundo setup, update config/lazy.lua for colorscheme, update lua/config/nvim-cmp.lua with cmdline sources, update lua/options.lua with undofile and cursorline settings. Add lua/plugins/auto-save.lua, lua/plugins/fundo.lua, and lua/plugins/lspsaga.lua to project.

--- init.lua
+++ init.lua
@@ -2,3 +2,5 @@ require("config.lazy")
 require('options')
 require('lsp')
 require('lualine').setup()
+require('fundo').setup()
+
--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -77,3 +77,12 @@ cmp.setup({
       { name = 'path' },        -- For path completion
   })
 })
+
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
