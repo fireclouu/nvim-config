ff92a226f12558659d1b371f5bad365a84e299e3
fireclouu <frcl@frcl.online>
2025-02-26 22:19:37 +0800

    update and fixes shell script

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Make sure python3 is installed
 
+echo "Updating pm caches"
+apt update -y && apt upgrade -y
 echo "Preparing virtual env..."
 if [ ! -d "$HOME/venv" ]; then
     mkdir -p "$HOME/venv"
@@ -23,7 +25,10 @@ source "$HOME/venv/myenv/bin/activate"
 python3 -m pip install pyright ripgrep
 if [ $? -ne 0 ]; then
     echo "Since pip fail, we check if ripgrep is available on default pm"
-    apt-get install ripgrep
+    apt-get install ripgrep -y
 fi
 
 npm install -g bash-language-server vscode-html-languageserver-bin vscode-css-languageserver-bin intelephense typescript-language-server
+
+apt-get install lua-language-server -y
+# brew install lua-language-server
